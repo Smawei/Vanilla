38e47873660ae71c6c07be626df3db3404db7e60
mawei <1061309048@qq.com>
2020-11-23 13:59:04 +0800

    子应用的urls

--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from book.views import index
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',index),
+    # path('index/',index),
+    path('', include('book.urls')),
 ]
